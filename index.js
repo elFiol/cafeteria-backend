35fb510bf21b0011ab79b05ddbf1f472efce5855
Fiol Thiago <fiolthiago9@gmail.com>
2024-03-05 18:48:46 -0300

    añadimos el puerto de backend

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 import express from "express";
 
 console.log("hola mundo")
-// configurar un puerto
+//1 - configurar un puerto
 
+const app = express()
+// crear una variable
+app.set("port", 4000);
+app.listen(app.get("port"), ()=>{
+    console.log("estoy en el puerto " + app.get("port"))
+})
+
+//2 - configurar los middleware del proyecto
+
+//3 - configurar las rutas
