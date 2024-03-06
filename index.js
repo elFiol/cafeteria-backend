20462ce98825b377f8f95fecb08116d5204575ac
Fiol Thiago <fiolthiago9@gmail.com>
2024-03-06 10:05:18 -0300

    agregamos el midleware para el index.html

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import 'dotenv/config'; // permite procesar variables de entornos
 import morgan from "morgan";
+import {fileURLToPath} from 'url';
+import path from "path";
 
 console.log("hola mundo")
 //1 - configurar un puerto
@@ -18,6 +20,15 @@ app.use(cors()) //permitir conexiones remotas
 app.use(morgan('dev')) // nos da info extra en la terminal 
 app.use(express.json())// interpretar datos en formato json
 app.use(express.urlencoded({extended: true}))// ayuda a interpretar los datos del body del request
-// falta configurar el index.html
-
+// configurar el index.html
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+// console.log(__dirname)
+// console.log(path.join(__dirname, 'public'))
+app.use(express.static(path.join(__dirname, 'public')))
 //3 - configurar las rutas
+app.get('/', (req, res)=>{
+    console.log("alguien solicito algo")
+    // falta configurar la respuesta 
+    res.send("respuesta desde el backend de cafesito")
+})
