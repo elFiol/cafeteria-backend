538ec47516dbb7319c406562bb28446176bf12ed
Fiol Thiago <fiolthiago9@gmail.com>
2024-03-18 21:51:10 -0300

    añadios la funcion de editar el producto

--- src/controller/productos.controller.js
+++ src/controller/productos.controller.js
@@ -27,6 +27,44 @@ export const obtenerProducto = async(req,res) => {
     }
 }
 
+export const editarProducto = async(req, res) => {
+    try{
+        // extraemos el id y buscamos producto
+        const buscarProducto = await Producto.findById(req.params.id)
+        // si no encuentroel id responder con un mensaje de error
+        if(!buscarProducto){
+            return res.status(404).json({mensaje: "no se pudo editar el el producto, el id es incorrecto"})
+        }
+        // pedir a la BD modificar el producto que tiene tal id, con los datos
+        await Producto.findByIdAndUpdate(req.params.id, req.body)
+        res.status(200).json({mensage: "el producto fue modificado exitosamente"})
+    }catch(error){
+        console.log(error)
+        res.status(500).json({
+            mensaje: "ocurrio un error al querer intentar editar el producto"
+        })
+    }
+}
+
+// export const editarProducto = async(req, res) => {
+//     try{
+//         // extraemos el id y buscamos producto
+//         const buscarProducto = await Producto.findById(req.params.id)
+//         // si no encuentroel id responder con un mensaje de error
+//         if(!buscarProducto){
+//             return res.status(404).json({mensaje: "no se pudo editar el el producto, el id es incorrecto"})
+//         }
+//         // pedir a la BD modificar el producto que tiene tal id, con los datos
+//         await Producto.findByIdAndUpdate(req.params.id, req.body)
+//         res.status(200).json({mensage: "el producto fue modificado exitosamente"})
+//     }catch(error){
+//         console.log(error)
+//         res.status(500).json({
+//             mensaje: "ocurrio un error al querer intentar editar el producto"
+//         })
+//     }
+// }
+
 export const crearProductos = async(req, res) => {
     try{
         // verificar los datos del body
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { listarProductos, crearProductos, obtenerProducto } from "../controller/productos.controller.js";
+import { listarProductos, crearProductos, obtenerProducto, editarProducto } from "../controller/productos.controller.js";
 
 const Productosrouter = Router();
 
-Productosrouter.route('/productos').get(listarProductos).post(crearProductos)
-Productosrouter.route("/producto/:id").get(obtenerProducto)
+Productosrouter.route('/productos').get(listarProductos).post(crearProductos);
+Productosrouter.route("/producto/:id").get(obtenerProducto).put(editarProducto).delete();
 
 export default Productosrouter
